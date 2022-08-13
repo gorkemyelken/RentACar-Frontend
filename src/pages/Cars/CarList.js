ccf126bde98b6901189555d182dbb737b3b0c3df
Görkem <gorkem_yelken@hotmail.com>
2022-08-13 21:02:14 +0300

    CarList filtre eklendi.

--- src/pages/Cars/CarList.js
+++ src/pages/Cars/CarList.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Container, Image } from 'semantic-ui-react'
+import { Card, Container, Image, Grid, Form, Button } from 'semantic-ui-react'
 import CarService from '../../services/carService';
-import './CarList.css';
 
 export default function CarList() {
 
@@ -16,27 +15,58 @@ export default function CarList() {
 
     return (
         <div>
-            <Container className='carsContainer'>
-                <Card.Group itemsPerRow="3">
-                    {cars.map((car) => (
-                    
-                                <Card raised key={car.carId}>
-                                    <Image circular src={car.carImages[0].imagePath} />
-                                    <Card.Content>
-                                        <Card.Header>
-                                            {car.carName}
-                                        </Card.Header>
-                                        <Card.Description>
-                                            {car.description}
-                                        </Card.Description>
-                                        <br />
-                                        <Card.Content extra>
-                                        Daily Price: {car.dailyPrice} <br/>
-                                        Model Year: {car.modelYear}
+            <Container>
+                <Grid divided='vertically'>
+                    <Grid.Row>
+                        <Grid.Column width={13}>
+                            <Card.Group itemsPerRow="3">
+                                {cars.map((car) => (
+                                    <Card raised key={car.carId}>
+                                        <Image circular size="huge" src={car.carImages[0].imagePath} />
+                                        <Card.Content>
+                                            <Card.Header>
+                                                {car.carName}
+                                            </Card.Header>
+                                            <Card.Description>
+                                                {car.description}
+                                            </Card.Description>
+                                            <br />
+                                            <Card.Content extra>
+                                                Daily Price: {car.dailyPrice} <br />
+                                                Model Year: {car.modelYear}
+                                            </Card.Content>
                                         </Card.Content>
-                                    </Card.Content>
-                                </Card>))}
-                </Card.Group>
+                                    </Card>))}
+                            </Card.Group>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Form>
+                                <Form.Select
+                                    name="brand"
+                                    placeholder="Brand"
+                                />
+                                <Form.Select
+                                    name="color"
+                                    placeholder="Color"
+                                />
+                                <h3>Daily Price</h3>
+                                <Form.Group widths="2">
+                                    <Form.Input fluid label='Min' />
+                                    <Form.Input fluid label='Max' />
+                                </Form.Group>
+                                <h3>Model Year</h3>
+                                <Form.Group widths="2">
+                                    <Form.Input fluid label='Min' />
+                                    <Form.Input fluid label='Max' />
+                                </Form.Group>
+                                <br />
+                                <Button circular fluid color="green" content="Filter" />
+                                <br />
+                                <Button circular fluid content="Clear Filter" />
+                            </Form>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Container>
         </div>
     );
