da737567a911bdbcbe1826216af158620d1d14fd
Görkem <gorkem_yelken@hotmail.com>
2022-08-26 17:25:58 +0300

    Brand add validations ve errors eklendi.

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import CustomerService from '../services/customerService'
 
-function SignUp() {
+export default function SignUp() {
+  let customerService = new CustomerService();
+
+  const {handleSubmit, handleChange, values, errors, touched, handleBlur}
   return (
     <div>SignUp</div>
   )
 }
-
-export default SignUp
