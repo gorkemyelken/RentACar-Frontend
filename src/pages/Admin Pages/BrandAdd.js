adced81953da557b4a7eea7a6338d56d83ffd309
Görkem <gorkem_yelken@hotmail.com>
2022-08-26 20:29:40 +0300

    Sign Up eklendi.

--- src/pages/Admin Pages/BrandAdd.js
+++ src/pages/Admin Pages/BrandAdd.js
@@ -3,29 +3,58 @@ import { useFormik } from "formik";
 import { Container, Grid, Form, Input, Button, Label } from "semantic-ui-react";
 import BrandService from "../../services/brandService";
 import * as yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function BrandAdd() {
   let brandService = new BrandService();
 
+  const notify = () =>
+    toast.success("Brand added!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   const validationSchema = yup.object().shape({
-    brandName: yup.string().required('Required field.'),
-    brandImagePath: yup.string().required('Required field.')
+    brandName: yup.string().required("Required field."),
+    brandImagePath: yup.string().required("Required field."),
   });
-  
-  const { handleSubmit, handleChange, values, errors, touched, handleBlur } = useFormik({
-    initialValues: {
-      brandName: "",
-      brandImagePath: "",
-    },
-    onSubmit: (values) => {
-      console.log(values);
-      brandService.add(values);
-    },
-    validationSchema
-  });
+
+  const { handleSubmit, handleChange, values, errors, touched, handleBlur } =
+    useFormik({
+      initialValues: {
+        brandName: "",
+        brandImagePath: "",
+      },
+      onSubmit: (values, { resetForm }) => {
+        console.log(values);
+        brandService.add(values);
+        if (brandService.add(values)) {
+          resetForm();
+          notify();
+        }
+      },
+      validationSchema,
+    });
 
   return (
     <Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <br />
       <br />
       <h2>Add A Brand</h2>
@@ -33,7 +62,7 @@ export default function BrandAdd() {
       <Grid centered>
         <Grid.Row>
           <Grid.Column width={8}>
-            <Form onSubmit={handleSubmit} className="form">
+            <Form onSubmit={handleSubmit}>
               <h3>Brand Name</h3>
               <Input
                 fluid
@@ -42,7 +71,11 @@ export default function BrandAdd() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {errors.brandName && touched.brandName && (<Label basic color='red' pointing='above'>{errors.brandName}</Label>)}
+              {errors.brandName && touched.brandName && (
+                <Label basic color="red" pointing="above">
+                  {errors.brandName}
+                </Label>
+              )}
               <h3>Brand Logo</h3>
               <Input
                 fluid
@@ -51,12 +84,16 @@ export default function BrandAdd() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {errors.brandImagePath && touched.brandImagePath && (<Label  basic color='red' pointing='above'>{errors.brandImagePath}</Label>)}
-              <br /><br/>
+              {errors.brandImagePath && touched.brandImagePath && (
+                <Label basic color="red" pointing="above">
+                  {errors.brandImagePath}
+                </Label>
+              )}
+              <br />
+              <br />
               <Button fluid color="green" type="submit">
                 Submit
               </Button>
-
               <code>{JSON.stringify(values)}</code>
             </Form>
           </Grid.Column>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,246 @@
-import React from 'react'
-import CustomerService from '../services/customerService'
+import React from "react";
+import { useFormik } from "formik";
+import { Container, Grid, Form, Input, Button, Label } from "semantic-ui-react";
+import CustomerService from "../services/customerService";
+import * as yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function SignUp() {
   let customerService = new CustomerService();
 
-  const {handleSubmit, handleChange, values, errors, touched, handleBlur}
+  const notify = () =>
+    toast.success("You have successfully registered!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+  const generateYearOptions = () => {
+    const arr = [];
+
+    const startYear = 1900;
+    const endYear = 2022;
+
+    for (let i = endYear; i >= startYear; i--) {
+      arr.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+
+    return arr;
+  };
+
+  const validationSchema = yup.object().shape({
+    firstName: yup.string().required("Required field."),
+    lastName: yup.string().required("Required field."),
+    nationalityNumber: yup
+      .string()
+      .required("Required field.")
+      .length(11, "Nationality number must be 11 character."),
+    email: yup
+      .string()
+      .required("Required field.")
+      .email("Email must be a valid email."),
+    password: yup
+      .string()
+      .required("Required field.")
+      .min(6, "Password must be at least 6 characters.")
+      .max(14, "Password must be at most 14 characters."),
+    confirmPassword: yup
+      .string()
+      .required("Required field.")
+      .min(6, "Password must be at least 6 characters.")
+      .max(14, "Password must be at most 14 characters.")
+      .oneOf([yup.ref("password"), null], "Passwords must match."),
+    birthYear: yup.number().required("Required field.").positive(),
+  });
+
+  const { handleSubmit, handleChange, values, errors, touched, handleBlur } =
+    useFormik({
+      initialValues: {
+        firstName: "",
+        lastName: "",
+        nationalityNumber: "",
+        email: "",
+        birthYear: "",
+        password: "",
+        confirmPassword: "",
+      },
+      onSubmit: (values, { resetForm }) => {
+        console.log(values);
+        customerService.add(values);
+        if (customerService.add(values)) {
+          resetForm();
+          notify();
+        }
+      },
+      validationSchema,
+    });
+
   return (
-    <div>SignUp</div>
-  )
+    <Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <br />
+      <br />
+      <h2>Sign Up</h2>
+      <hr />
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Form onSubmit={handleSubmit}>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={8}>
+                    <h3>First Name</h3>
+                    <Input
+                      fluid
+                      name="firstName"
+                      placeholder="Example"
+                      value={values.firstName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.firstName && touched.firstName && (
+                      <Label basic color="red" pointing="above">
+                        {errors.firstName}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <h3>Last Name</h3>
+                    <Input
+                      fluid
+                      name="lastName"
+                      placeholder="Example"
+                      value={values.lastName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.lastName && touched.lastName && (
+                      <Label basic color="red" pointing="above">
+                        {errors.lastName}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={8}>
+                    <h3>Nationality Number</h3>
+                    <Input
+                      fluid
+                      name="nationalityNumber"
+                      placeholder="XXXXXXXXXXX"
+                      value={values.nationalityNumber}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.nationalityNumber && touched.nationalityNumber && (
+                      <Label basic color="red" pointing="above">
+                        {errors.nationalityNumber}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <h3>Birth Year</h3>
+                    <select
+                      name="birthYear"
+                      placeholder="YYYY"
+                      value={values.birthYear}
+                      onChange={handleChange}
+                    >
+                      <option>Year</option>
+                      {generateYearOptions()}
+                    </select>
+                    {errors.birthYear && touched.birthYear && (
+                      <Label basic color="red" pointing="above">
+                        {errors.birthYear}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+              <h3>Email</h3>
+              <Input
+                fluid
+                name="email"
+                placeholder="example@example.com"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.email && touched.email && (
+                <Label basic color="red" pointing="above">
+                  {errors.email}
+                </Label>
+              )}
+              <br />
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={8}>
+                    <h3>Password</h3>
+                    <Input
+                      fluid
+                      type="password"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password && (
+                      <Label basic color="red" pointing="above">
+                        {errors.password}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <h3>Confirm Password</h3>
+                    <Input
+                      fluid
+                      type="password"
+                      name="confirmPassword"
+                      value={values.confirmPassword}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.confirmPassword && touched.confirmPassword && (
+                      <Label basic color="red" pointing="above">
+                        {errors.confirmPassword}
+                      </Label>
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+              <br />
+              <br />
+              <Button fluid color="green" type="submit">
+                Submit
+              </Button>
+
+              <code>{JSON.stringify(values)}</code>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
 }
